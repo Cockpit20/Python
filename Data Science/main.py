a8a19e91db6a6a756d3cd6cbfc0d25db103615a0
Soumyadeep Chandra <soumyadeepchandra2002@gmail.com>
2022-06-22 14:40:57 +0530

    Enter

---
+++ Data Science/main.py
@@ -0,0 +1,25 @@
+# Multiple Assignment Operator
+height,length,width=3,6,2
+volume=height*length*width
+print(volume)
+
+# Integers and Floats
+print(3/2)
+
+print(type(3))
+print(type(2.5))
+
+print(float(3))
+print(int(28.9))
+
+# Floating Point Precision
+print(0.23+0.23+0.23+0.23+0.23+0.23+0.23+0.23+0.23+0.23+0.23+0.23+0.23+0.23+0.23+0.23+0.23+0.23+0.23+0.23+0.23+0.23+0.23+0.23+0.23+0.23+0.23+0.23+0.23+0.23==6.9)
+print(0.23*30==6.9)
+
+#Boolean, Comparison and Logical Operators
+a=3>1
+print(a)
+
+rent=1200
+is_affordable= rent>1000 and rent<2000
+print(is_affordable)
